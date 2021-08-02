01c20d2b931a26fe021d9fde328309b4b258ccc0
donghee <dhk010111@gmail.com>
2021-08-02 14:45:36 +0900

    first commit

---
+++ items/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.contrib.auth.decorators import login_required
+
+
+def main(request):
+    items = Post.objects.all()
+    return render(request, 'items/home.html', {'items':items})
+
+def new(request):
+    return render(request, 'items/new.html')
+
+def create(request):
+    if request.method=="POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        image = request.FILES.get('image')
+        user = request.user
+        Post.objects.create(title=title, content=content, image=image,user=user)
+    return redirect('main')
+
+def show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.view_count = post.view_count+1
+    post.save()
+    return render(request, 'items/show.html', {'post':post})
+
+
+#삭제하기
+def delete(request,post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+    return redirect('main')
+
+#
